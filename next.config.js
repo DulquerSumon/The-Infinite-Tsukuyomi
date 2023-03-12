7743638235b6966596bc69e52c011b40cb3a3438
DulquerSumon <m.dulquer.sumon@gmail.com>
2023-03-12 23:51:21 +0600

    image changed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    loader: "akamai",
+    path: "",
+    unoptimized: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
